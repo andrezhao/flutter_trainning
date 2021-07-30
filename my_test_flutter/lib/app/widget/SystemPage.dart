b6d57087205750da4d0c3d4f3cff1b3db6ec7dc1
andrezhao INWC <tyou.keikou@inwc.co.jp>
2021-07-30 19:31:10 +0900

    no message

--- my_test_flutter/lib/app/widget/SystemPage.dart
+++ my_test_flutter/lib/app/widget/SystemPage.dart
@@ -5,44 +5,71 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_test_flutter/app/Bean/address.dart';
 import 'package:my_test_flutter/app/Bean/new_tree.dart';
-
 import 'package:my_test_flutter/app/Http/http_manager.dart';
 import 'package:my_test_flutter/app/Http/request_api.dart';
-
 import 'package:my_test_flutter/app/utils/strings.dart';
 
 class SystemPage extends GetView<SystemController> {
-
   @override
   var _systemController = Get.put<SystemController>(SystemController());
 
-
-
-
   Widget build(BuildContext context) {
-    _systemController.getSystemInfoTitleList();
-    List<Widget> _getData(){
+    /* _systemController.getSystemInfoTitleList().then((value) =>
+        print("2222+${value}"));*/
+    /* List<Widget> _getData() {
       List<Widget> list = [];
-      for(var i=0;i<_systemController._tipItems.length;i++){
+      for (var i = 0; i < list.length; i++) {
         list.add(ListTile(
-          title: Text("${_systemController._tipItems[i].toString()}"),
+          title: Text("${list[i].toString()}"),
         ));
       }
       return list;
-    }
-   print("55555${_systemController._tipItems.length}");
+    }*/
 
     return new Scaffold(
-      appBar: AppBar(automaticallyImplyLeading: false,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
         centerTitle: true, // 中央寄せを解除//设置没有返回按钮
         title: new Text(SystemPageTitle),
-
       ),
-      body: Center(child: ListView (children: _getData(),),
+      body: FutureBuilder(
+        future: _systemController.getSystemInfoTitleList(),
+        builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
 
+              return Text('还没有开始网络请求');
+            case ConnectionState.active:
+
+              return Text('ConnectionState.active');
+            case ConnectionState.waiting:
+
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.done:
+
+              print("${snapshot.data}");
+              if (snapshot.hasError) return Text('Error: ${snapshot.error}');
+              return ListView(children: getData(snapshot.data));
+          }
+        },
       ),
     );
   }
+}
+
+
+
+List<Widget> getData(List tlist) {
+
+  List<Widget> list = [];
+  for (var i = 0; i < tlist.length; i++) {
+    list.add(ListTile(
+      title: Text("${tlist[i].toString()}"),
+    ));
+  }
+  return list;
 }
 
 class SystemController extends GetxController {
@@ -50,25 +77,18 @@ class SystemController extends GetxController {
 
   List<String> get tipItems => _tipItems;
 
-
-    void getSystemInfoTitleList()  async{
+  getSystemInfoTitleList() async {
     Map<String, dynamic> map = Map();
-    List<String> _list =[];
-    var response = await HttpManager.instance.get(
-        "https://www.wanandroid.com/tree/json");
+    List<String> _list = [];
+    var response =
+        await HttpManager.instance.get("https://www.wanandroid.com/tree/json");
     map = json.decode(response.toString());
-     //planListData  = map2['data'];
     var treeEntity = TreeEntity.fromJson(map);
 
-   // print("1111 + ${treeEntity.data.toString()}");
     for (int i = 0; i < treeEntity.data!.length; i++) {
       treeEntity.data![i].isExpanded = false;
-      print("1111111 + ${ treeEntity.data![i].name}");
       _tipItems.add(treeEntity.data![i].name);
     }
-    print("1111 + ${_tipItems.length}");
+    return _tipItems;
   }
-
-
-
 }
