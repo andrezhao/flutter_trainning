76f87bb9c217081c3eafb8768ad9ba197dcf555d
andrezhao <tyou.keikou@inwc.co.jp>
2021-08-15 16:09:54 +0900

    3Dpicture

--- my_test_flutter/lib/app/modules/other_module/other_controller.dart
+++ my_test_flutter/lib/app/modules/other_module/other_controller.dart
@@ -1,3 +1,6 @@
+
+
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 /**
  * GetX Template Generator - fb.com/htngu.99
@@ -8,4 +11,14 @@ class OtherController extends GetxController{
   var _obj = ''.obs;
   set obj(value) => _obj.value = value;
   get obj => _obj.value;
+
+  Image getImage(String s) {
+    return Image.asset(
+      "assets/icon/$s",
+      width: Get.width-10,
+      height: Get.height-250,
+      fit: BoxFit.cover,
+      scale: 2.0,
+    );
+  }
 }
--- my_test_flutter/lib/app/modules/other_module/other_page.dart
+++ my_test_flutter/lib/app/modules/other_module/other_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_interactional_widget/flutter_interactional_widget.dart';
 import 'package:my_test_flutter/app/modules/other_module/other_controller.dart';
 /**
  * GetX Template Generator - fb.com/htngu.99
@@ -7,12 +8,44 @@ import 'package:my_test_flutter/app/modules/other_module/other_controller.dart';
 
 class OtherPage extends GetView<OtherController> {
   @override
+  var _controller = Get.put<OtherController>(OtherController());
+
   Widget build(BuildContext context) {
     Get.lazyPut(() => OtherController());
+
     return Scaffold(
-      body: Container(
-        child: Obx(()=>Container(child: Text(controller.obj),)),
+      body: InteractionalWidget(
+        maxAngleX: 30,
+        maxAngleY: 80,
+        width: Get.width,
+        height: Get.height,
+        middleScale: 1,
+        foregroundScale: 1.1,
+        backgroundScale: 1.1,
+        backgroundWidget: backgroundWidget(),
+        //middleWidget: middleWidget(),
+        foregroundWidget: foregroundWidget(),
       ),
+    );
+  }
+
+  Widget backgroundWidget() {
+    return Container(
+      child: _controller.getImage('back.png'),
+    );
+  }
+
+  //"assets/icon/village.jpg",
+  Widget foregroundWidget() {
+    return Container(
+      child: _controller.getImage('fore.png'),
+    );
+  }
+
+  Widget middleWidget() {
+    // return Center(child: Text('hello'));
+    return Container(
+      child: _controller.getImage('mid.png'),
     );
   }
 }
